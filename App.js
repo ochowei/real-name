88b72a66bdbe0db75420ad689540a2b66841af53
William <ochowei@gmail.com>
2021-05-30 15:17:06 +0800

    add tailwind

--- App.js
+++ App.js
@@ -1,13 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View, Button, Image } from 'react-native';
+import tailwind from 'tailwind-rn';
 
 export default function App() {
+  console.log(tailwind('bg-blue-200 px-3 py-1 rounded-full'));
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <View  style={tailwind('h-full')}>
+		<View style={tailwind('mt-10 pt-20 items-center')}>
+    <Image
+        style={ {width: 238, height: 151, resizeMode: 'center' }} 
+        source={require('./assets/logo.png')}
+      />
+			<View style={tailwind('bg-blue-200 px-3 py-1 rounded-full')}>
+				<Text style={tailwind('bg-blue-500 text-white font-bold py-2 px-4 border border-blue-700 rounded')}>
+					Hello Tailwind
+				</Text>        
+			</View>
+      <Button title="Learn More" style={tailwind('bg-blue-500 text-white font-bold py-2 px-4 border border-blue-700 rounded')}>
+        Button
+      </Button>
+		</View>
+	</View>
   );
 }
 
